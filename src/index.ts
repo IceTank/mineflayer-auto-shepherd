7bd7ccb1aa4970c6cc66aa662760a9b6f0161445
IceTank <niklaslehle@gmail.com>
2021-12-23 16:38:17 +0100

    Code refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as mineflayer from "mineflayer"
 import dotenv from "dotenv"
-import { goals, Movements, pathfinder } from "mineflayer-pathfinder"
+import { Movements, pathfinder } from "mineflayer-pathfinder"
 const mineflayerViewer = require('prismarine-viewer').mineflayer
 import autoeat from "mineflayer-auto-eat";
 import MinecraftData from "minecraft-data";
@@ -10,9 +10,9 @@ import inventoryViewer = require('mineflayer-web-inventory')
 const wait = require('util').promisify(setTimeout)
 dotenv.config()
 import { makeBot } from "mineflayer-proxy-inspector";
+import { inject as autoShepherdPlugin } from "./plugins/autoShepherd";
 import path from "path";
 import { promises as fs } from "fs";
-import { Item } from "prismarine-item";
 import readline from "readline";
 
 const chatLog = path.join(__dirname, '../chat.txt')
@@ -47,40 +47,33 @@ async function init() {
       bot.end()
     }, 120000)
   }
+
+  const handleReconnect = () => {
+    console.info('Restarting in 30sec')
+    setTimeout(() => {
+      init()
+        .catch(console.error)
+    }, 30000)
+  }
     
   bot = conn.bot
   bot.on('error', console.error)
   bot.on('kicked', (reason) => console.info('Kicked for reason', reason))
-  bot.on('end', () => {
-    // @ts-ignore
-    if (bot.viewer?.close) bot.viewer.close()
-    console.info('Disconnected')
-    if (afkIntervalHandle) clearInterval(afkIntervalHandle)
-    setTimeout(() => {
-      console.info('Restarting bot')
-      init()
-        .catch(console.error)
-    }, 15000)
-    if (actionTimeout) clearTimeout(actionTimeout)
-  })
+  bot.on('end', handleReconnect)
   bot.on('message', (chatMessage) => {
     // console.info(chatMessage.toAnsi())
     fs.appendFile(chatLog, chatMessage.toString() + '\n')
       .catch(console.error)
   })
 
-  resetActionTimeout()
-
-  let shouldShear = true
-  let shouldEat = false
-  let shouldDeposit = false
-
-  bot.loadPlugins([pathfinder, autoeat])
+  bot.loadPlugins([pathfinder, autoeat, autoShepherdPlugin])
   
   // @ts-ignore
   bot.autoEat.disable()
 
   await once(bot, 'spawn')
+
+  resetActionTimeout()
   mineflayerViewer(bot, { port: 3000 })
   inventoryViewer(bot, { port: 3001 })
   console.info('Spawned')
@@ -107,173 +100,64 @@ async function init() {
     bot.swingArm(undefined)
   }, 15000)
 
-  bot.on('health', () => {
+  bot.on('end', () => {
+    console.info('Disconnected')
+    if (afkIntervalHandle) clearInterval(afkIntervalHandle)
+    if (actionTimeout) clearTimeout(actionTimeout)
+    // @ts-ignore
+    if (bot.viewer?.close) bot.viewer.close()
+    rl.close()
+    bot.autoShepherd.stopSheering()
+    bot.removeAllListeners()
+  })
+  
+  bot.on('health', async () => {
     if (bot.health < 10) {
       console.warn('Took to much damage logging off')
       process.exit(1)
     }
     // @ts-ignore
     if (bot.food < 16 && ! bot.autoEat.isEating) {
-      shouldEat = true
+      await bot.autoShepherd.stopSheering()
+      console.info('Starting to eat')
+      await new Promise<void>((resolve) => {
+        // @ts-ignore
+        bot.autoEat.eat((err: Error) => {
+          if (err) console.error(err)
+          resolve()
+        })
+      })
+      bot.autoShepherd.startSheering()
     }
+  })
+
+  bot.autoShepherd.emitter.on('cycle', () => {
+    resetActionTimeout()
   })
 
   rl.on('line', (line) => {
     line = line.trim()
-    if (line === 'start' && shouldShear === false) {
-      shouldShear = true
-      startShearing()
-        .catch(console.error)
+    if (line === 'start') {
+      bot.autoShepherd.startSheering()
       return
     } else if (line === 'stop') {
-      shouldShear = false
+      bot.autoShepherd.stopSheering()
     } else if (line === 'test') {
       console.info(conn.receivingPclients.length)
     } else if (line === 'deposit') {
-      shouldDeposit = true
+      bot.autoShepherd.stopSheering().then(async () => {
+        await bot.autoShepherd.depositItems()
+        bot.autoShepherd.startSheering()
+      })
     }
   })
 
-  bot.on('end', () => rl.close())
-
-  const startShearing = async () => {
-    console.info('Starting')
-    while (shouldShear) {
-      resetActionTimeout()
-      if (shouldEat) {
-        console.info('Starting to eat')
-        await new Promise<void>((resolve) => {
-          // @ts-ignore
-          bot.autoEat.eat((err: Error) => {
-            if (err) console.error(err)
-            shouldEat = false
-            resolve()
-          })
-        })
-      }
-      const shears = bot.inventory.items().find(i => i.name.includes('shears'))
-      if (!shears) {
-        console.info('No more shears left')
-        shouldShear = false
-        continue
-      }
-      // console.info('Getting wool')
-      await getWool()
-      await wait(1000)
-      // console.info('Getting dropped items')
-      await getItems()
-      await wait(1000)
-      if (bot.inventory.emptySlotCount() < 2 || shouldDeposit) await depositItems()
-      await wait(1000)
-    }
-  }
-
-  const depositItems = async () => {
-    shouldDeposit = false
-    const woolId = mcData.itemsByName.wool.id
-    const tryDeposit = async (window: mineflayer.Chest): Promise<boolean> => {
-      // @ts-ignore
-      while (window.findInventoryItem(woolId) !== null) {
-        // @ts-expect-error
-        const emptySlot = window.firstEmptyContainerSlot() as number | null
-        if (emptySlot === null) return false
-        // @ts-ignore
-        const invItem = window.findInventoryItem(woolId) as Item
-        if (invItem === null) return true
-        await bot.clickWindow(invItem.slot, 0, 0)
-        await wait(200)
-        await bot.clickWindow(emptySlot, 0, 0)
-        await wait(200)
-      }
-      return true
-    }
-
-    const chests = bot.findBlocks({
-      matching: [mcData.blocksByName.chest.id, mcData.blocksByName.trapped_chest.id],
-      count: 64
-    })
-    if (chests.length === 0) return
-    // console.info(chests)
-    const depositSpots = chests.filter(c => bot.blockAt(c.offset(0, -1, 0))?.type === woolId)
-    // console.info(depositSpots)
-
-    for (const d of depositSpots) {
-      let window: mineflayer.Chest | undefined = undefined
-      try {
-        await bot.pathfinder.goto(new goals.GoalGetToBlock(d.x, d.y, d.z))
-        const containerBlock = bot.blockAt(d)
-        if (!containerBlock) continue
-        window = await bot.openChest(containerBlock)
-        const res = await tryDeposit(window)
-        window.close()
-        if (res) return true
-      } catch (err) {
-        console.error(err)
-        if (window) window.close()
-        break
-      } 
-    }
-    if (bot.inventory.emptySlotCount() < 2) {
-      console.info('No more space to put wool')
-      process.exit(0)
-    } else {
-      return true
-    }
-  }
-
-  startShearing()
-}
-
-async function getItems() {
-  const droppedItems = Object.values(bot.entities).filter(e => {
-    return e.name === 'item' && e.position.distanceTo(bot.entity.position) < 30
+  rl.on('SIGINT', () => {
+    bot.removeListener('end', handleReconnect)
+    setInterval(() => bot.end())
   })
-  if (droppedItems.length === 0) return
-  droppedItems.sort((a, b) => {
-    return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
-  })
-  while (droppedItems.length !== 0) {
-    droppedItems.sort((a, b) => {
-      return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
-    })
-    const item = droppedItems.pop()
-    if (!item) break
-    try {
-      await bot.pathfinder.goto(new goals.GoalFollow(item, 1))
-    } catch (err) {
-      if ((err as Error).name !== 'NoPath') console.error(err)
-    }
-  }
-}
 
-async function getWool() {
-  const unSheeredSheep = Object.values(bot.entities).filter(e => {
-    return e.name === 'sheep' && e.position.distanceTo(bot.entity.position) < 45 && (e.metadata[13] as unknown as number) < 16 && (e.metadata[12] as unknown as boolean) == false
-  })
-  unSheeredSheep.sort((a, b) => {
-    return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
-  })
-  if (unSheeredSheep.length === 0) return
-  while (unSheeredSheep.length !== 0) {
-    unSheeredSheep.sort((a, b) => {
-      return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
-    })
-    const sheep = unSheeredSheep.pop()
-    if (!sheep) break
-    try {
-      await bot.pathfinder.goto(new goals.GoalFollow(sheep, 1))
-      const shears = bot.inventory.items().find(i => i.name.includes('shears'))
-      if (!shears) {
-        console.error('No more shears left')
-        return
-      }
-      await bot.equip(shears.type, 'hand')
-      await wait(100)
-      await bot.activateEntity(sheep)
-    } catch (err) {
-      if ((err as Error).name !== 'NoPath') console.error(err)
-    }
-  }
+  bot.autoShepherd.startSheering()
 }
 
 init()
---
+++ src/plugins/autoShepherd.ts
@@ -0,0 +1,189 @@
+import { Bot, BotOptions, Chest } from "mineflayer";
+import { goals } from "mineflayer-pathfinder";
+import Data from "minecraft-data";
+import { promisify } from "util";
+import { EventEmitter } from "stream";
+import { once } from "events";
+const wait = promisify(setTimeout)
+
+declare module 'mineflayer' {
+  interface Bot {
+    autoShepherd: AutoShepherd
+  }
+}
+
+interface AutoShepherd {
+  getItems: () => Promise<void>
+  getWool: () => Promise<void>
+  depositItems: () => Promise<boolean>
+  startSheering: () => void
+  stopSheering: () => Promise<void>
+  isRunning: () => boolean
+
+  emitter: AutoShepherdEmitter
+}
+
+interface AutoShepherdEmitter extends EventEmitter {
+  on(event: 'cycle', listener: () => void): this
+}
+
+export function inject(bot: Bot, options: BotOptions): void {
+  let shouldDeposit: boolean = false
+  let shouldStop: boolean = false
+  let isRunning = false
+  const mcData = Data(bot.version)
+
+  bot.autoShepherd = {
+    getItems: async () => {
+      const droppedItems = Object.values(bot.entities).filter(e => {
+        return e.name === 'item' && e.position.distanceTo(bot.entity.position) < 30
+      })
+      if (droppedItems.length === 0) return
+      droppedItems.sort((a, b) => {
+        return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
+      })
+      while (droppedItems.length !== 0) {
+        droppedItems.sort((a, b) => {
+          return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
+        })
+        const item = droppedItems.pop()
+        if (!item) break
+        try {
+          const { x, y, z } = item.position.floored()
+          await bot.pathfinder.goto(new goals.GoalBlock(x, y, z))
+        } catch (err) {
+          if ((err as Error).name !== 'NoPath') console.error(err)
+        }
+      }
+    },
+    getWool: async () => {
+      const unSheeredSheep = Object.values(bot.entities).filter(e => {
+        return e.name === 'sheep' && e.position.distanceTo(bot.entity.position) < 45 && (e.metadata[13] as unknown as number) < 16 && (e.metadata[12] as unknown as boolean) == false
+      })
+      unSheeredSheep.sort((a, b) => {
+        return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
+      })
+      if (unSheeredSheep.length === 0) return
+      while (unSheeredSheep.length !== 0) {
+        unSheeredSheep.sort((a, b) => {
+          return b.position.distanceTo(bot.entity.position) - a.position.distanceTo(bot.entity.position)
+        })
+        const sheep = unSheeredSheep.pop()
+        if (!sheep) break
+        try {
+          await bot.pathfinder.goto(new goals.GoalFollow(sheep, 1))
+          const shears = bot.inventory.items().find(i => i.name.includes('shears'))
+          if (!shears) {
+            console.error('No more shears left')
+            return
+          }
+          await bot.equip(shears.type, 'hand')
+          await wait(100)
+          await bot.activateEntity(sheep)
+        } catch (err) {
+          if ((err as Error).name !== 'NoPath') console.error(err)
+        }
+      }
+    },
+    depositItems: async () => {
+      shouldDeposit = false
+      const woolId = mcData.itemsByName.wool.id
+      let depositedItems = 0
+      /** Deposit items in the chest with manual item clicking to counter lag/anti cheat */
+      const tryDeposit = async (window: Chest): Promise<boolean> => {
+        // @ts-ignore
+        while (window.findInventoryItem(woolId) !== null) {
+          // @ts-expect-error
+          const emptySlot = window.firstEmptyContainerSlot() as number | null
+          if (emptySlot === null) return false
+          // @ts-ignore
+          const invItem = window.findInventoryItem(woolId) as Item
+          if (invItem === null) return true
+          depositedItems += invItem.count
+          await bot.clickWindow(invItem.slot, 0, 0)
+          await wait(200)
+          await bot.clickWindow(emptySlot, 0, 0)
+          await wait(200)
+        }
+        return true
+      }
+  
+      const chests = bot.findBlocks({
+        matching: [mcData.blocksByName.chest.id, mcData.blocksByName.trapped_chest.id],
+        count: 64
+      })
+      if (chests.length === 0) return false
+      // console.info(chests)
+      const depositSpots = chests.filter(c => bot.blockAt(c.offset(0, -1, 0))?.type === woolId)
+      // console.info(depositSpots)
+  
+      for (const d of depositSpots) {
+        let window: Chest | undefined = undefined
+        try {
+          await bot.pathfinder.goto(new goals.GoalGetToBlock(d.x, d.y, d.z))
+          const containerBlock = bot.blockAt(d)
+          if (!containerBlock) continue
+          console.info(`Depositing items into chest at ${containerBlock.position.toString()}`)
+          window = await bot.openChest(containerBlock)
+          const res = await tryDeposit(window)
+          window.close()
+          if (res) {
+            console.info(`Deposited ${depositedItems} items (${Math.floor(depositedItems / 64)} stacks + ${depositedItems % 64}) into chests`)
+            return true
+          }
+        } catch (err) {
+          console.error(err)
+          if (window) window.close()
+          break
+        } 
+      }
+      if (bot.inventory.emptySlotCount() < 2) {
+        console.info('No more space to put wool')
+        process.exit(0)
+      } else {
+        return true
+      }
+    },
+    isRunning: () => {
+      return isRunning
+    },
+    startSheering: () => {
+      console.info('Starting')
+      startSheering()
+        .catch(console.error)
+    },
+    stopSheering: async () => {
+      shouldStop = true
+      await once(bot.autoShepherd.emitter, 'cycle')
+    },
+    emitter: new EventEmitter()
+  } 
+
+  const startSheering = async () => {
+    cycle()
+  }
+
+  const cycle = async () => {
+    if (shouldStop) {
+      bot.autoShepherd.emitter.emit('cycle')
+      return
+    }
+    const shears = bot.inventory.items().find(i => i.name.includes('shears'))
+    if (!shears) {
+      console.info('No more shears left')
+      process.exit(0)
+    }
+    // console.info('Getting wool')
+    await bot.autoShepherd.getWool()
+    await wait(1000)
+    // console.info('Getting dropped items')
+    await bot.autoShepherd.getItems()
+    await wait(1000)
+    if (bot.inventory.emptySlotCount() < 2) await bot.autoShepherd.depositItems()
+    bot.autoShepherd.emitter.emit('cycle')
+    setTimeout(() => {
+      cycle()
+        .catch(console.error)
+    }, 1000)
+  } 
+}
